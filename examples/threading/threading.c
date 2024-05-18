0de0c1177b60c9d730f0fa4e48c7937f54030e36
Ubuntu <ubuntu@ip-172-31-7-229.ap-south-1.compute.internal>
2024-05-18 07:32:35 +0000

    modified threading.c and threading.h

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,14 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_data_t = (struct thread_data *) thread_param;
+    DEBUG_LOG("Thread_entered\n");
+    thread_data_t->thread_complete_success = false;
+    usleep(thread_data_t->wait_to_obtain_ms *1000);
+    pthread_mutex_lock(thread_data_t->mutex);
+    usleep(thread_data_t->wait_to_release_ms *1000);
+    pthread_mutex_unlock(thread_data_t->mutex);
+    thread_data_t->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +36,18 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    bool ret = false;
+    struct thread_data *thread_data_t = (struct thread_data*)malloc(1 * sizeof(struct thread_data));
+    thread_data_t->mutex = mutex;
+    thread_data_t->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data_t->wait_to_release_ms = wait_to_release_ms;
+    
+    int result = pthread_create(thread, NULL, threadfunc, (void*)thread_data_t);
+    if(result == 0)
+        ret = true;
+    else
+        ret = false;
+
+    return ret;
 }
 
